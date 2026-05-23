bb91573f9ecf9835532ee41e4e515b38b3d913a7
davidtav <david.mclaurel@outlook.com.br>
2026-05-23 19:28:49 -0300

    adiciona funcionalidade de envio assíncrono de URL com tratamento de resposta

---
+++ wwwroot/js/index.js
@@ -0,0 +1,50 @@
+document.getElementById("btnSubmit").addEventListener("click", function (e) {
+  e.preventDefault();
+  handleSubmitAsync();
+});
+
+document.getElementById("url").addEventListener("keyup", function (evt) {
+  if (evt.code === "Enter") {
+    event.preventDefault();
+    handleSubmitAsync();
+  }
+});
+
+function handleSubmitAsync() {
+  const url = document.getElementById("url").value;
+
+  let json = { url: url };
+
+  const headers = {
+    "content-type": "application/json",
+  };
+
+  fetch("/urls", {
+    method: "post",
+    body: JSON.stringify(json),
+    headers: headers,
+  })
+    //.then(apiResult => apiResult.json())
+    .then((apiResult) => {
+      return new Promise((resolve) =>
+        apiResult.json().then((json) => {
+          return resolve({
+            ok: apiResult.ok,
+            status: apiResult.status,
+            json: json,
+          });
+        }),
+      );
+    })
+    .then(({ json, ok, status }) => {
+      console.log(json);
+
+      if (ok) {
+        const anchor = `<a href=${json.shortUrl} target="_blank">${json.shortUrl}</a>`;
+
+        document.getElementById("urlResult").innerHTML = anchor;
+      } else {
+        alert(json.errorMessage);
+      }
+    });
+}
