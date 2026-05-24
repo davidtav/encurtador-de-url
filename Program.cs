7f1ffc3d07bb39f58b0bde345d2dbe4a20d07695
davidtav <david.mclaurel@outlook.com.br>
2026-05-23 22:55:50 -0300

    refatora configuração do aplicativo para incluir Swagger em ambiente de desenvolvimento

--- Program.cs
+++ Program.cs
@@ -4,12 +4,21 @@ using LiteDB;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCarter();
-builder.Services.AddSingleton<ILiteDatabase, LiteDatabase>(x=> new LiteDatabase("short.db"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ILiteDatabase, LiteDatabase>(x => new LiteDatabase("short.db"));
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseExceptionHandler();
+    app.UseHsts();
 }
 
 app.UseStaticFiles();
